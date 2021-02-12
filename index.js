8036812d3385d6c06bafb4118716f309b33564be
valentinesamuel <valentinesamuel2580@gmail.com>
2021-02-12 11:38:56 -0800

    (FEAT): Finished the header section

---
+++ index.js
@@ -0,0 +1,12 @@
+var link = document.createElement("li");
+var a = document.createElement("a");
+var node = document.createTextNode("Sign Up");
+a.appendChild(node);
+link.appendChild(a);
+var element = document.querySelector("#menu");
+
+function toggleMobileMenu(){
+      element.appendChild(link);
+document.querySelector('#menu').classList.toggle('active')
+document.querySelector('.mobile-bar').classList.toggle('active')
+}
