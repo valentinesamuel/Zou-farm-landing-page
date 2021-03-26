5060073241d75376183b2819e186105b128c2c87
valentinesamuel <valentinesamuel2580@gmail.com>
2021-03-26 23:43:16 +0100

    (TIP): Learnt how to get token from a url

--- index.js
+++ index.js
@@ -11,4 +11,10 @@ function toggleMobileMenu() {
       document.querySelector('.mobile-bar').classList.toggle('active')
 }
 
-// arr = []
+//GETTING THE STRING FROM THE API
+const token = '#acessToken=mysupersecterkey&name=valentine&surname=samuel';
+// const slug = str.split('#').pop();
+const slug = token.substring(0, token.indexOf('&'));
+const newSlug = slug.split('=').pop()
+console.log(newSlug);
+console.log(token);
